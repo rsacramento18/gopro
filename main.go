91393f3bb8fabd6015544c5ed76c142257b19831
sacramento <ricardo.sacramento18@gmail.com>
2024-07-24 07:58:35 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"time"
+
+	"tinygo.org/x/bluetooth"
+)
+
+var adapter = bluetooth.DefaultAdapter
+
+func main() {
+	print("enabling")
+	must("enable BLE stack", adapter.Enable())
+	adv := adapter.DefaultAdvertisement()
+	must("config adv", adv.Configure(bluetooth.AdvertisementOptions{
+		LocalName: "Go Bluetooth",
+		ManufacturerData: []bluetooth.ManufacturerDataElement{
+			{CompanyID: 0xFEA6, Data: []byte{0x01, 0x02}},
+		},
+	}))
+
+	must("start adv", adv.Start())
+
+	println("advertising...")
+	address, _ := adapter.Address()
+
+	for {
+		println("Go Bluetooth /", address.MAC.String())
+		time.Sleep(time.Second)
+	}
+}
+
+func must(action string, err error) {
+	if err != nil {
+		panic("failed to " + action + ": " + err.Error())
+	}
+}
