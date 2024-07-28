e893858678b4a184be39f459d07f3af5dc2c2289
sacramento <ricardo.sacramento18@gmail.com>
2024-07-28 22:33:27 +0100

    adding connect controller and adding a small menu to control actions

---
+++ controller/controller.go
@@ -0,0 +1,67 @@
+package controller
+
+import (
+	"fmt"
+	"time"
+
+	"tinygo.org/x/bluetooth"
+)
+
+const (
+	BLE_SERVICE_GOPRO                         = 0xFEA6
+	BLE_CHARACTERISTIC_GOPRO_COMMAND          = "b5f90072-aa8d-11e3-9046-0002a5d5c51b"
+	BLE_CHARACTERISTIC_GOPRO_COMMAND_RESPONSE = "b5f90073-aa8d-11e3-9046-0002a5d5c51b"
+)
+
+type Controller struct {
+	Adapter bluetooth.Adapter
+	Scans   []bluetooth.Address
+}
+
+func NewController() Controller {
+	adapter := bluetooth.DefaultAdapter
+	must("enable BLE stack", adapter.Enable())
+	return Controller{*adapter, []bluetooth.Address{}}
+}
+
+func (controller *Controller) Scan() {
+	fmt.Println("scanning...")
+	count := 0
+	err := controller.Adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+		if device.HasServiceUUID(bluetooth.New16BitUUID(BLE_SERVICE_GOPRO)) {
+			println("found device:", device.Address.String(), device.LocalName())
+			controller.Scans = append(controller.Scans, device.Address)
+		}
+		count++
+		time.Sleep(1 * time.Second)
+		if count == 5 {
+			controller.Adapter.StopScan()
+		}
+	})
+	must("start scan", err)
+	fmt.Println("finished scanning")
+}
+
+func (controller Controller) PrintScans() {
+	for index, node := range controller.Scans {
+		fmt.Println(index, "-", node.String())
+	}
+}
+
+func (controller Controller) Connect(address bluetooth.Address) bluetooth.Device {
+	res, err := controller.Adapter.Connect(address, bluetooth.ConnectionParams{})
+	go func() {
+		if err != nil {
+			println("error connection:", err.Error())
+			return
+		}
+	}()
+
+	return res
+}
+
+func must(action string, err error) {
+	if err != nil {
+		panic("failed to " + action + ": " + err.Error())
+	}
+}
--- main.go
+++ main.go
@@ -3,94 +3,116 @@ package main
 import (
 	"fmt"
 
-	"tinygo.org/x/bluetooth"
+	"gopro/controller"
 )
 
-const (
-	BLE_SERVICE_GOPRO                         = 0xFEA6
-	BLE_CHARACTERISTIC_GOPRO_COMMAND          = "b5f90072-aa8d-11e3-9046-0002a5d5c51b"
-	BLE_CHARACTERISTIC_GOPRO_COMMAND_RESPONSE = "b5f90073-aa8d-11e3-9046-0002a5d5c51b"
-)
+// const (
+// 	BLE_SERVICE_GOPRO                         = 0xFEA6
+// 	BLE_CHARACTERISTIC_GOPRO_COMMAND          = "b5f90072-aa8d-11e3-9046-0002a5d5c51b"
+// 	BLE_CHARACTERISTIC_GOPRO_COMMAND_RESPONSE = "b5f90073-aa8d-11e3-9046-0002a5d5c51b"
+// )
 
-var adapter = bluetooth.DefaultAdapter
+// var adapter = bluetooth.DefaultAdapter
 
 func main() {
-	// Enable BLE interface.
-	must("enable BLE stack", adapter.Enable())
+	// // Enable BLE interface.
+	// must("enable BLE stack", adapter.Enable())
 
-	// Start scanning.
-	println("scanning...")
-	err := adapter.Scan(onScan)
-	must("start scan", err)
-}
+	// // Start scanning.
+	// println("scanning...")
+	// err := adapter.Scan(onScan)
+	// must("start scan", err)
 
-func onScan(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
-	if device.HasServiceUUID(bluetooth.New16BitUUID(BLE_SERVICE_GOPRO)) {
-		println("found device:", device.Address.String(), device.RSSI, device.LocalName())
+	ct := controller.NewController()
+	var operation int
+	fmt.Println("Select 1")
+	fmt.Println("======================")
+	fmt.Println("1 - Scan Devices")
 
-		go func() {
-			res, err := adapter.Connect(device.Address, bluetooth.ConnectionParams{})
-			if err != nil {
-				println("error connection:", err.Error())
-				return
-			}
-			onConnect(device, res)
-		}()
+	fmt.Scanf("%d", &operation)
+	fmt.Println("this was the number", operation)
+
+	switch operation {
+	case 1:
+		ct.Scan()
+	}
+
+	if len(ct.Scans) > 0 {
+		fmt.Println("Connect to device:")
+		fmt.Println("==================")
+		ct.PrintScans()
+		fmt.Scanf("%d", &operation)
+
+		ct.Connect(ct.Scans[operation])
 	}
 }
 
-func onConnect(scanResult bluetooth.ScanResult, device bluetooth.Device) {
-	fmt.Println("connected:", scanResult.Address.String(), scanResult.LocalName())
+// func onScan(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
+// 	if device.HasServiceUUID(bluetooth.New16BitUUID(BLE_SERVICE_GOPRO)) {
+// 		println("found device:", device.Address.String(), device.RSSI, device.LocalName())
 
-	// Get a list of services
-	services, err := device.DiscoverServices([]bluetooth.UUID{
-		bluetooth.New16BitUUID(BLE_SERVICE_GOPRO),
-	})
-	if err != nil {
-		fmt.Println("error getting services:", err.Error())
-		return
-	}
+// 		go func() {
+// 			res, err := adapter.Connect(device.Address, bluetooth.ConnectionParams{})
+// 			if err != nil {
+// 				println("error connection:", err.Error())
+// 				return
+// 			}
+// 			onConnect(device, res)
+// 		}()
+// 	}
+// }
 
-	for _, service := range services {
-		if service.UUID() == bluetooth.New16BitUUID(BLE_SERVICE_GOPRO) {
-			sendCommand, _ := bluetooth.ParseUUID(BLE_CHARACTERISTIC_GOPRO_COMMAND)
-			commandRes, _ := bluetooth.ParseUUID(BLE_CHARACTERISTIC_GOPRO_COMMAND_RESPONSE)
-			characteristics, err := service.DiscoverCharacteristics([]bluetooth.UUID{
-				sendCommand, commandRes,
-			})
-			if err != nil {
-				fmt.Println("error getting characteristics:", err.Error())
-				return
-			}
+// func onConnect(scanResult bluetooth.ScanResult, device bluetooth.Device) {
+// 	fmt.Println("connected:", scanResult.Address.String(), scanResult.LocalName())
 
-			for _, characteristic := range characteristics {
-				if characteristic.UUID() == sendCommand {
-					shutterOn := []byte{0x03, 0x01, 0x01, 0x01}
-					_, err := characteristic.WriteWithoutResponse(shutterOn)
-					if err != nil {
-						fmt.Println("send command", err.Error())
-						return
-					}
-				}
-				if characteristic.UUID() == commandRes {
-					err := characteristic.EnableNotifications(notification)
-					if err != nil {
-						fmt.Println("error enabling notifications:", err.Error())
-						return
-					}
-				}
-			}
-		}
-	}
-}
+// 	// Get a list of services
+// 	services, err := device.DiscoverServices([]bluetooth.UUID{
+// 		bluetooth.New16BitUUID(BLE_SERVICE_GOPRO),
+// 	})
+// 	if err != nil {
+// 		fmt.Println("error getting services:", err.Error())
+// 		return
+// 	}
 
-func notification(buf []byte) {
-	fmt.Println("received: %x", buf)
-}
+// 	for _, service := range services {
+// 		if service.UUID() == bluetooth.New16BitUUID(BLE_SERVICE_GOPRO) {
+// 			sendCommand, _ := bluetooth.ParseUUID(BLE_CHARACTERISTIC_GOPRO_COMMAND)
+// 			commandRes, _ := bluetooth.ParseUUID(BLE_CHARACTERISTIC_GOPRO_COMMAND_RESPONSE)
+// 			characteristics, err := service.DiscoverCharacteristics([]bluetooth.UUID{
+// 				sendCommand, commandRes,
+// 			})
+// 			if err != nil {
+// 				fmt.Println("error getting characteristics:", err.Error())
+// 				return
+// 			}
 
-func must(action string, err error) {
-	if err != nil {
-		panic("failed to " + action + ": " + err.Error())
-	}
-}
+// 			for _, characteristic := range characteristics {
+// 				if characteristic.UUID() == sendCommand {
+// 					shutterOn := []byte{0x03, 0x05, 0x01, 0x01}
+// 					_, err := characteristic.WriteWithoutResponse(shutterOn)
+// 					if err != nil {
+// 						fmt.Println("send command", err.Error())
+// 						return
+// 					}
+// 				}
+// 				if characteristic.UUID() == commandRes {
+// 					err := characteristic.EnableNotifications(notification)
+// 					if err != nil {
+// 						fmt.Println("error enabling notifications:", err.Error())
+// 						return
+// 					}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
+// func notification(buf []byte) {
+// 	fmt.Println("received: %x", buf)
+// }
+
+// func must(action string, err error) {
+// 	if err != nil {
+// 		panic("failed to " + action + ": " + err.Error())
+// 	}
+// }
